0ac9a7ec513be9de249a526fe01025f335f43e17
KumarB95 <kumarbalingal@gmail.com>
2021-09-18 00:26:55 +0530

    Modified : Added player and checking winner between two.

--- src/com/bridgelabz/SnakeAndLadderGame.java
+++ src/com/bridgelabz/SnakeAndLadderGame.java
@@ -4,42 +4,75 @@ import java.util.Random;
 
 public class SnakeAndLadderGame {
 
+    int rollOutput,option,numberOfTimesDieRolled=0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game ");
-        int playerPosition=0;
-        System.out.println("Starting player position is "+playerPosition+" ; Player rolls die");
+        System.out.println("Player 1 :"+player1());
+        System.out.println("Player 2 :"+player2());
+
+        if (player1()>player2()){
+            System.out.println("Player1 one won !");
+        }
+        else {
+            System.out.println("Player2 one won !");
+        }
+    }
+
+
+    private static int player1() {
+        int player1Position=0,rollOutput,numberOfTimesDieRolledbyPlayer1=0;
         Random rand=new Random();
-        int rollOutput,option,numberOfTimesDieRolled=0;
-
-
-        while (playerPosition!=100){
+        while (player1Position!=100){
             rollOutput = rand.nextInt(6);
-            //System.out.println("Die output : " + (rollOutput+1));
-            //checking option No play, Snake and Ladder
-            option=rand.nextInt(3);
+            int option=rand.nextInt(3);
 
             switch (option) {
                 case 0:
-                    //System.out.println("No Play");
-                    playerPosition += 0;
+                    player1Position += 0;
                     break;
                 case 1:
-                    //System.out.println("Snake");
-                    playerPosition = playerPosition - rollOutput + 1;
-                    if (playerPosition < 0) {
-                        playerPosition = 0;
+                    player1Position = player1Position - rollOutput + 1;
+                    if (player1Position < 0) {
+                        player1Position = 0;
                     }
                     break;
                 case 2:
-                    //System.out.println("Ladder");
-                    playerPosition += rollOutput + 1;
+                    player1Position += rollOutput + 1;
                     break;
             }
-            if (playerPosition == 100){
-                System.out.println("Player at winning position :"+playerPosition);
+            if (player1Position == 100){
                 break;}
-            numberOfTimesDieRolled++;
+            numberOfTimesDieRolledbyPlayer1++;
         }
-        System.out.println("Number of times die Rolled to Win game : "+numberOfTimesDieRolled);
+        return numberOfTimesDieRolledbyPlayer1;
+    }
+
+    private static int player2() {
+        int player2Position=0,rollOutput,numberOfTimesDieRolledbyPlayer2=0;
+        Random rand=new Random();
+        while (player2Position!=100){
+            rollOutput = rand.nextInt(6);
+            int option=rand.nextInt(3);
+
+            switch (option) {
+                case 0:
+                    player2Position += 0;
+                    break;
+                case 1:
+                    player2Position = player2Position - rollOutput + 1;
+                    if (player2Position < 0) {
+                        player2Position = 0;
+                    }
+                    break;
+                case 2:
+                    player2Position += rollOutput + 1;
+                    break;
+            }
+            if (player2Position == 100){
+                break;}
+            numberOfTimesDieRolledbyPlayer2++;
+        }
+        return numberOfTimesDieRolledbyPlayer2;
     }
 }
