93a5ba1c6ad077a61b08bf6290b0e5c270740017
KumarB95 <kumarbalingal@gmail.com>
2021-09-17 19:53:14 +0530

    Modified : Die rolls till player reaches exact winning position 100.

--- src/com/bridgelabz/SnakeAndLadderGame.java
+++ src/com/bridgelabz/SnakeAndLadderGame.java
@@ -12,9 +12,9 @@ public class SnakeAndLadderGame {
         int rollOutput,option;
 
 
-        while (playerPosition<100) {
+        while (playerPosition!=100){
             rollOutput = rand.nextInt(6);
-            System.out.println("Die output : " + (rollOutput+1));
+            //System.out.println("Die output : " + (rollOutput+1));
             //checking option No play, Snake and Ladder
             option=rand.nextInt(3);
 
@@ -22,24 +22,23 @@ public class SnakeAndLadderGame {
                 case 0:
                     System.out.println("No Play");
                     playerPosition += 0;
-                    System.out.println(playerPosition);
                     break;
                 case 1:
                     System.out.println("Snake");
                     playerPosition = playerPosition - rollOutput + 1;
                     if (playerPosition < 0) {
                         playerPosition = 0;
-                        System.out.println(playerPosition);
-                    } else {
-                        System.out.println(playerPosition);
                     }
                     break;
                 case 2:
                     System.out.println("Ladder");
                     playerPosition += rollOutput + 1;
-                    System.out.println(playerPosition);
                     break;
             }
+            if (playerPosition == 100){
+                System.out.println(playerPosition);
+                break;}
+
         }
     }
 }
